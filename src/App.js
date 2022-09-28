dbb32132f84a3ffb8da9b825ee84ce09eb8397e3
Santosh Phaiju <santoshphaiju062@gmail.com>
2022-09-28 17:08:14 +0545

    learning some basics

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, styled, Typography } from "@mui/material";
+import SettingsIcon from "@mui/icons-material/Settings";
+import { YouTube } from "@mui/icons-material";
+import { Add } from "@mui/icons-material";
 
 function App() {
+
+  const BlueButton = styled(Button)(({theme})=>({
+    background: theme.palette.primary.main,
+    color: "white",
+    margin: 5,
+    "&:hover":{
+      background: '#0000aa'
+    }
+  }))
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello world</h1>
+      <Button variant="text">Text</Button>
+      <Button
+        startIcon={<SettingsIcon />}
+        endIcon={<YouTube />}
+        variant="contained"
+        color="primary"
+        size="small"
+      >
+        Contained
+      </Button>
+      <Button variant="outlined" color="primary">
+        Outlined
+      </Button>
+      <Typography variant="h1" component="h2" sx={{
+        color: "blueviolet",
+        textAlign: "center",
+        fontWeight: "bold",
+        fontFamily: "monospace",
+        margin: "2px 3px"
+      }}>
+        h1. Heading
+      </Typography>
+      <Button variant="contained" startIcon={<Add />} color="success">
+        Add New Post
+      </Button>
+
+      <BlueButton variant="outlined" color="primary">This is my button</BlueButton>
+      <BlueButton>This is my another button</BlueButton>
+
     </div>
   );
 }
